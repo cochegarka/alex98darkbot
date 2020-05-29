84b098d4cb33bf618ab0bb66f87f8c0df3fecc25
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-05-29 14:28:22 +0700

    bobobo bababa

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,21 @@ bot.command('plain', async ctx => {
   const text = (ctx.message?.text || '').substr('/plain'.length);
   const estimation = await new PlainTextEstimation(text).measure();
 
-  await ctx.reply(estimation.toString());
+  let opinion = '';
+
+  if (estimation <= 0.25) {
+    opinion = 'По-моему, текст написан человеком';
+  } else if (estimation <= 0.5) {
+    opinion = 'Мне кажется, текст достачно человекочитаемый';
+  } else if (estimation <= 1) {
+    opinion = 'Скорее всего написан человеком, но есть небольшие сомнения';
+  } else if (estimation <= 10) {
+    opinion = 'Мне кажется, текст достаточно человеконечитаемый';
+  } else if (isNaN(estimation)) {
+    opinion = 'По-моему, текст ДАННЫЕ УДАЛЕНЫ!';
+  }
+
+  await ctx.reply(`Оценка текста: ${estimation}\n${opinion}`);
 });
 
 app.get('/', (_, res) => {
